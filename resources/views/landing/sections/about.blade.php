c9e9afa43876a2b728cc0d3a9ae3f4c0acc45845
carbolivariA <syntidev@gmail.com>
2026-03-12 06:35:05 -0400

    fix: about bg-surface -> bg-background para homologar con servicios

--- resources/views/landing/sections/about.blade.php
+++ resources/views/landing/sections/about.blade.php
@@ -15,7 +15,7 @@
         : null;
 @endphp
 
-<section id="about" class="relative py-10 sm:py-16 lg:py-24 bg-surface overflow-hidden">
+<section id="about" class="relative py-10 sm:py-16 lg:py-24 bg-background overflow-hidden">
 
     {{-- ░░ ADORNOS GEOMÉTRICOS ░░ --}}
     {{-- Círculo grande top-right --}}
