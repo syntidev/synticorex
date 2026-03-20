9b142f040281711c0f4a0d61a660e3f4040100aa
carbolivariA <syntidev@gmail.com>
2026-03-20 06:10:17 -0400

    fix: debug entrypoint con set -x

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 set -e
+set -x
 
-# Crear config nginx con PORT correcto
 cat > /etc/nginx/conf.d/default.conf << EOF
 server {
     listen ${PORT:-8080};
     root /app/public;
     index index.php;
-
     location / {
         try_files \$uri \$uri/ /index.php?\$query_string;
     }
-
     location ~ \.php$ {
         fastcgi_pass 127.0.0.1:9000;
         fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
@@ -20,13 +18,7 @@ server {
 }
 EOF
 
-# Migraciones
 php artisan migrate --force
-
-# Iniciar php-fpm
 php-fpm -D
-
 sleep 1
-
-# Iniciar nginx
 nginx -g 'daemon off;'
