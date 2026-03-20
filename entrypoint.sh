e6178e33e44a8658079dd4610b60dba453b34cc5
carbolivariA <syntidev@gmail.com>
2026-03-20 05:36:55 -0400

    fix: nginx conf.d y entrypoint corregido

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 set -e
 
-# Sustituir PORT en nginx config
-envsubst '${PORT}' < /etc/nginx/sites-enabled/default > /tmp/nginx.conf
+envsubst '${PORT}' < /etc/nginx/sites-enabled/default > /etc/nginx/conf.d/default.conf
 
-# Correr migraciones
 php artisan migrate --force
 
-# Iniciar php-fpm
 php-fpm -D
 
 sleep 1
 
-# Iniciar nginx
-nginx -c /tmp/nginx.conf -g 'daemon off;'
+nginx -g 'daemon off;'
