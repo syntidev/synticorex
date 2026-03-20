cb764181bff249e88de686492c080f35111e2d86
carbolivariA <syntidev@gmail.com>
2026-03-20 05:48:07 -0400

    fix: entrypoint con heredoc para nginx conf.d

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,32 @@
 #!/bin/sh
 set -e
 
-envsubst '${PORT}' < /etc/nginx/sites-enabled/default > /etc/nginx/conf.d/default.conf
+# Crear config nginx con PORT correcto
+cat > /etc/nginx/conf.d/default.conf << EOF
+server {
+    listen ${PORT:-8080};
+    root /app/public;
+    index index.php;
 
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+    }
+
+    location ~ \.php$ {
+        fastcgi_pass 127.0.0.1:9000;
+        fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}
+EOF
+
+# Migraciones
 php artisan migrate --force
 
+# Iniciar php-fpm
 php-fpm -D
 
 sleep 1
 
+# Iniciar nginx
 nginx -g 'daemon off;'
